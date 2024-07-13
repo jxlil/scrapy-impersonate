fff4d4f75030c7bd1920c8db3aed94180d629665
Jalil SA <61639983+jxlil@users.noreply.github.com>
2024-07-12 20:10:40 -0600

    update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name="scrapy-impersonate",
-    version="1.3.0",
+    version="1.3.1",
     author="Jalil SA (jxlil)",
     description="Scrapy download handler that can impersonate browser fingerprints",
     license="MIT",
