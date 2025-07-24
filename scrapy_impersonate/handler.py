01080c3ff34a1fe0321ad1d55310876cfb7df2b1
Jalil SA <61639983+jxlil@users.noreply.github.com>
2025-07-23 23:40:36 -0600

    fix: handle redirects

--- scrapy_impersonate/handler.py
+++ scrapy_impersonate/handler.py
@@ -1,13 +1,11 @@
 import time
 from typing import Type, TypeVar
-from urllib.parse import urljoin, urlparse
 
 from curl_cffi.requests import AsyncSession
 from scrapy.core.downloader.handlers.http11 import (
     HTTP11DownloadHandler as HTTPDownloadHandler,
 )
 from scrapy.crawler import Crawler
-from scrapy.exceptions import IgnoreRequest
 from scrapy.http.headers import Headers
 from scrapy.http.request import Request
 from scrapy.http.response import Response
@@ -52,14 +50,6 @@ class ImpersonateDownloadHandler(HTTPDownloadHandler):
         headers = Headers(response.headers.multi_items())
         headers.pop("Content-Encoding", None)
 
-        if (
-            300 <= response.status_code < 400
-            and b"Location" in headers
-            and not request.meta.get("dont_redirect")
-        ):
-            new_request = self._handle_redirect(request, headers, spider)
-            return self._download_request(new_request, spider)  # type: ignore
-
         respcls = responsetypes.from_args(
             headers=headers,
             url=response.url,
@@ -77,28 +67,3 @@ class ImpersonateDownloadHandler(HTTPDownloadHandler):
 
         resp.meta["download_latency"] = download_latency
         return resp
-
-    @staticmethod
-    def _handle_redirect(request: Request, headers: Headers, spider: Spider) -> Request:
-        allowed_domains = getattr(spider, "allowed_domains", [])
-        if allowed_domains:
-            request.meta["dont_redirect"] = True
-
-        location = headers.get("Location") or b""
-        location = location.decode()
-
-        redirected_url = (
-            location if location.startswith("http") else urljoin(request.url, location)
-        )
-
-        domain = urlparse(redirected_url).netloc
-        if not any(domain == d for d in allowed_domains):  # type: ignore
-            spider.logger.warning(
-                f"Filtered offsite request to %(domain)r: %(request)s",
-                {"domain": domain, "request": request},
-                extra={"spider": spider},
-            )
-
-            raise IgnoreRequest(f"Redirected to disallowed domain: {redirected_url}")
-
-        return request.replace(url=redirected_url)
